3ebde4c89a213ec92f74aafe00d79fdd6c70d976
Keiichiro Kuroki <shinmili@users.noreply.github.com>
2019-02-17 18:36:09 +0900

    Statusを整形して表示できるようにする

---
+++ WpfApp2/StatusBlock.cs
@@ -0,0 +1,37 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Xaml;
+
+namespace WpfApp2
+{
+    class StatusBlock : TextBlock
+    {
+        public List<Inline> InlineList
+        {
+            get => (List<Inline>)GetValue(InlineListProperty);
+            set
+            {
+                SetValue(InlineListProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty InlineListProperty =
+            DependencyProperty.Register("InlineList", typeof(List<Inline>), typeof(StatusBlock), new PropertyMetadata(new PropertyChangedCallback(OnInlineListChanged)));
+
+        public static void OnInlineListChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var block = d as StatusBlock;
+            foreach (var inline in (List<Inline>)e.NewValue)
+            {
+                block.Inlines.Add(inline);
+            }
+        }
+    }
+}
---
+++ WpfApp2/StatusConverter.cs
@@ -0,0 +1,59 @@
+﻿using HtmlAgilityPack;
+using Mastonet.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Documents;
+
+namespace WpfApp2
+{
+    class StatusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml((string)value);
+            var root = doc.DocumentNode.SelectSingleNode("/p");
+            return root.ChildNodes.Select(ConvertSingleNode).ToList<Inline>();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Inline ConvertSingleNode(HtmlNode node)
+        {
+            if (node.NodeType == HtmlNodeType.Text)
+            {
+                return new Run(node.InnerText);
+            }
+            switch (node.Name)
+            {
+                case "a":
+                    var link = new Hyperlink();
+                    link.NavigateUri = new Uri(node.Attributes["href"].Value);
+                    foreach (var child in node.ChildNodes)
+                    {
+                        link.Inlines.Add(ConvertSingleNode(child));
+                    }
+                    return link;
+                case "br":
+                    return new LineBreak();
+                case "span":
+                    var span = new Span();
+                    foreach (var child in node.ChildNodes)
+                    {
+                        span.Inlines.Add(ConvertSingleNode(child));
+                    }
+                    return span;
+                default:
+                    return null;
+            }
+        }
+    }
+}
