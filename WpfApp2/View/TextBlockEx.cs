829b6606cebb358781fc9bb86c3f4f723bbfba5c
Keiichiro Kuroki <shinmili@users.noreply.github.com>
2019-04-06 01:37:54 +0900

    Custom Emojiの表示に対応

---
+++ WpfApp2/View/TextBlockEx.cs
@@ -0,0 +1,203 @@
+﻿using HtmlAgilityPack;
+using Mastonet.Entities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp2.View
+{
+    class TextBlockEx : TextBlock
+    {
+        public static readonly DependencyProperty ParseHTMLProperty =
+            DependencyProperty.Register(
+                nameof(ParseHTML),
+                typeof(bool),
+                typeof(TextBlockEx),
+                new FrameworkPropertyMetadata(false,
+                    (o, e) =>
+                    {
+                        TextBlockEx tb = (TextBlockEx)o;
+                        tb.Render(tb.Text, (bool)e.NewValue, tb.Emojis);
+                    }));
+
+        public bool ParseHTML
+        {
+            get => (bool)GetValue(ParseHTMLProperty);
+            set => SetValue(ParseHTMLProperty, value);
+        }
+
+        public static readonly DependencyProperty EmojisProperty =
+            DependencyProperty.Register(
+                nameof(Emojis),
+                typeof(IEnumerable<Emoji>),
+                typeof(TextBlockEx),
+                new FrameworkPropertyMetadata(new List<Emoji>(),
+                    (o, e) =>
+                    {
+                        TextBlockEx tb = (TextBlockEx)o;
+                        tb.Render(tb.Text, tb.ParseHTML, (IEnumerable<Emoji>)e.NewValue);
+                    }));
+
+        public IEnumerable<Emoji> Emojis
+        {
+            get => (IEnumerable<Emoji>)GetValue(EmojisProperty);
+            set => SetValue(EmojisProperty, value.ToList());
+        }
+
+        static TextBlockEx()
+        {
+            TextProperty.OverrideMetadata(typeof(TextBlockEx), new FrameworkPropertyMetadata(
+                (string)TextProperty.GetMetadata(typeof(TextBlock)).DefaultValue,
+                (o, e) =>
+                {
+                    TextBlockEx tb = (TextBlockEx)o;
+                    tb.Render((string)e.NewValue, tb.ParseHTML, tb.Emojis);
+                }));
+            FontSizeProperty.OverrideMetadata(typeof(TextBlockEx), new FrameworkPropertyMetadata(
+                (double)FontSizeProperty.GetMetadata(typeof(TextBlock)).DefaultValue,
+                (o, e) => ((TextBlockEx)o).OnFontSizeChanged((double)e.NewValue)));
+        }
+        public TextBlockEx() : base() { }
+
+        private void Render(string text, bool parseHTML, IEnumerable<Emoji> emojis)
+        {
+            Console.WriteLine($"text:{text}, parseHTML:{parseHTML}, emojis:{emojis?.Count()}");
+            Inlines.Clear();
+            if (parseHTML)
+            {
+                Inlines.AddRange(htmlToInlines(text).SelectMany(inline =>
+                {
+                    Run run = inline as Run;
+                    if (run == null) { return new List<Inline> { inline }; }
+                    else { return stringToInlines(run.Text, emojis); }
+                }));
+            }
+            else
+            {
+                Inlines.AddRange(stringToInlines(text, emojis));
+            }
+            OnFontSizeChanged(FontSize);
+        }
+
+        private void OnFontSizeChanged(double size)
+        {
+            foreach(Inline inline in Inlines)
+            {
+                var container = inline as InlineUIContainer;
+                if (container != null)
+                {
+                    ((Image)container.Child).Height = size;
+                }
+            }
+        }
+
+        private static IEnumerable<Inline> stringToInlines(string text, IEnumerable<Emoji> emojis)
+        {
+            int head = 0;
+            bool done = false;
+            while (!done)
+            {
+                int colon1 = text.IndexOf(':', head);
+                if (colon1 == -1)
+                {
+                    done = true;
+                }
+                else
+                {
+                    while (true)
+                    {
+                        int colon2 = text.IndexOf(':', colon1 + 1);
+                        if (colon2 == -1)
+                        {
+                            done = true;
+                            break;
+                        }
+                        Emoji e = emojis.FirstOrDefault(x => x.Shortcode == text.Substring(colon1 + 1, colon2 - colon1 - 1));
+                        if (e != null)
+                        {
+                            yield return new Run(text.Substring(head, colon1 - head));
+                            BitmapImage bi = new BitmapImage();
+                            bi.BeginInit();
+                            bi.UriSource = new Uri(e.Url);
+                            bi.EndInit();
+                            Image image = new Image() { Source = bi };
+                            RenderOptions.SetBitmapScalingMode(image, BitmapScalingMode.Fant);
+                            yield return new InlineUIContainer(image);
+                            head = colon2 + 1;
+                            break;
+                        }
+                        else
+                        {
+                            colon1 = colon2;
+                        }
+                    }
+                }
+            }
+            yield return new Run(text.Substring(head));
+        }
+
+        private static IEnumerable<Inline> htmlToInlines(string htmlText)
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(htmlText);
+            var pnodes = doc.DocumentNode.ChildNodes;
+
+            foreach (var (pnode, index) in pnodes.Select((v, i) => (v, i)))
+            {
+                if (index > 0)
+                {
+                    yield return new LineBreak();
+                    yield return new LineBreak();
+                }
+                foreach (var child in pnode.ChildNodes)
+                {
+                    foreach (var inline in htmlSingleNodeToInlines(child))
+                    {
+                        yield return inline;
+                    }
+                }
+            }
+        }
+
+        private static IEnumerable<Inline> htmlSingleNodeToInlines(HtmlNode node)
+        {
+            if (node.NodeType == HtmlNodeType.Text)
+            {
+                yield return new Run(HtmlEntity.DeEntitize(node.InnerText));
+            }
+            switch (node.Name)
+            {
+                case "a":
+                    var link = new Hyperlink();
+                    link.NavigateUri = new Uri(node.Attributes["href"].Value);
+                    link.RequestNavigate += (sender, eventArgs) => Process.Start(eventArgs.Uri.AbsoluteUri);
+                    foreach (var child in node.ChildNodes)
+                    {
+                        link.Inlines.AddRange(htmlSingleNodeToInlines(child));
+                    }
+                    yield return link;
+                    break;
+                case "br":
+                    yield return new LineBreak();
+                    break;
+                case "span":
+                    foreach (var child in node.ChildNodes)
+                    {
+                        foreach (var inline in htmlSingleNodeToInlines(child))
+                        {
+                            yield return inline;
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
--- WpfApp2/ViewModel/StatusViewModel.cs
+++ WpfApp2/ViewModel/StatusViewModel.cs
@@ -22,8 +22,6 @@ namespace WpfApp2
         public Status OriginalStatus { get; private set; }
         public string StaticAvatarUrl { get; }
         public string DisplayName { get; }
-        public List<Inline> ContentFlow { get; }
-        public List<Inline> ContentFlowOneLine { get; }
         public ReactiveProperty<bool> Deleted { get; }
 
         public StatusViewModel(Status s)
@@ -33,122 +31,6 @@ namespace WpfApp2
 
             StaticAvatarUrl = OriginalStatus.Account.StaticAvatarUrl;
             DisplayName = OriginalStatus.Account.DisplayName + (s.Reblog == null ? "" : $"(RT:{s.Account.AccountName})");
-            ContentFlow = ConvertHtmlToFlow(OriginalStatus.Content).ToList();
-            ContentFlowOneLine = ContentFlow.Select(x => x is LineBreak ? new Run (" ") : x).ToList();
         }
-
-        #region Emoji Processing
-
-        private static IEnumerable<Inline> UnfoldEmojis(string text, IEnumerable<Emoji> emojis)
-        {
-            List<(int i, Emoji e)> occs = FindEmojis(text, emojis)
-                .OrderBy(x => x.Item1)
-                .ToList();
-            if (occs.Count() == 0)
-            {
-                yield return new Run(text);
-            }
-            else
-            {
-                yield return new Run(text.Substring(0, occs[0].i));
-            }
-            for (int i = 0; i < occs.Count(); i++)
-            {
-                (int i, Emoji e) occ = occs[i];
-
-                yield return new InlineUIContainer(new Image { Source = new BitmapImage(new Uri(occ.e.StaticUrl)) });
-
-                int runStartIndex = occ.i + occ.e.Shortcode.Length + 2;
-                if (i + 1 < occs.Count())
-                {
-                    (int i, Emoji e) next = occs[i + 1];
-                    yield return new Run(text.Substring(runStartIndex, next.i - runStartIndex));
-                }
-                else
-                {
-                    yield return new Run(text.Substring(runStartIndex));
-                }
-            }
-        }
-
-        private static IEnumerable<(int, Emoji)> FindEmojis(string text, IEnumerable<Emoji> emojis)
-        {
-            foreach (Emoji emoji in emojis)
-            {
-                string emojiString = ":" + emoji.Shortcode + ":";
-                int searchStartIndex = 0;
-                while (true)
-                {
-                    searchStartIndex = text.IndexOf(emojiString, searchStartIndex + 1);
-                    if (searchStartIndex == -1)
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        yield return (searchStartIndex, emoji);
-                    }
-                }
-            }
-        }
-
-        #endregion
-        #region HTML Processing
-        private static IEnumerable<Inline> ConvertHtmlToFlow(string text)
-        {
-            var doc = new HtmlDocument();
-            doc.LoadHtml(text);
-            var pnodes = doc.DocumentNode.ChildNodes;
-
-            foreach (var (pnode, index) in pnodes.Select((v, i) => (v, i)))
-            {
-                if (index > 0)
-                {
-                    yield return new LineBreak();
-                    yield return new LineBreak();
-                }
-                foreach (var child in pnode.ChildNodes)
-                {
-                    foreach (var inline in ConvertSingleNode(child))
-                    {
-                        yield return inline;
-                    }
-                }
-            }
-        }
-
-        private static IEnumerable<Inline> ConvertSingleNode(HtmlNode node)
-        {
-            if (node.NodeType == HtmlNodeType.Text)
-            {
-                yield return new Run(HtmlEntity.DeEntitize(node.InnerText));
-            }
-            switch (node.Name)
-            {
-                case "a":
-                    var link = new Hyperlink();
-                    link.NavigateUri = new Uri(node.Attributes["href"].Value);
-                    link.RequestNavigate += (sender, eventArgs) => Process.Start(eventArgs.Uri.AbsoluteUri);
-                    foreach (var child in node.ChildNodes)
-                    {
-                        link.Inlines.AddRange(ConvertSingleNode(child));
-                    }
-                    yield return link;
-                    break;
-                case "br":
-                    yield return new LineBreak();
-                    break;
-                case "span":
-                    foreach (var child in node.ChildNodes)
-                    {
-                        foreach (var inline in ConvertSingleNode(child))
-                        {
-                            yield return inline;
-                        }
-                    }
-                    break;
-            }
-        }
-        #endregion
     }
 }
