10cfa8427a564ca37bb520533e3e86ffc6526df6
Keiichiro Kuroki <shinmili@users.noreply.github.com>
2019-04-19 18:44:20 +0900

    アカウントタブに表示項目を追加

--- WpfApp2/ViewModel/AccountTabViewModel.cs
+++ WpfApp2/ViewModel/AccountTabViewModel.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using WpfApp2.Model;
 
@@ -13,6 +14,9 @@ namespace WpfApp2.ViewModel
     class AccountTabViewModel : TabContentViewModelBase
     {
         public ReactiveProperty<Account> Account { get; } = new ReactiveProperty<Account>();
+        public ReactiveProperty<string> FullName { get; } = new ReactiveProperty<string>();
+        public ReactiveProperty<string> FollowingUrl { get; } = new ReactiveProperty<string>();
+        public ReactiveProperty<string> FollowersUrl { get; } = new ReactiveProperty<string>();
         public StatusesViewModel Statuses { get; }
 
         public AccountTabViewModel(AccountTabParameters param) : base(param)
@@ -25,6 +29,16 @@ namespace WpfApp2.ViewModel
         {
             var client = new MastodonClient(Properties.Settings.Default.AppRegistration, Properties.Settings.Default.Auth);
             Account.Value = await client.GetAccount(id);
+            FullName.Value = Account.Value.AccountName;
+            if (!FullName.Value.Contains('@'))
+            {
+                Regex regex = new Regex("^https://(?<domain>[^/]*)/");
+                Match match = regex.Match(Account.Value.ProfileUrl);
+                FullName.Value += '@' + match.Groups["domain"].Value;
+            }
+
+            FollowingUrl.Value = Account.Value.ProfileUrl.Replace("@", "users/") + "/following";
+            FollowersUrl.Value = Account.Value.ProfileUrl.Replace("@", "users/") + "/followers";
         }
     }
 }
